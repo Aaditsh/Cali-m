976b5b8ff6798f1f95a64094a051ace5f223f9c4
Aaditsh <66991661+Aaditsh@users.noreply.github.com>
2021-03-11 11:30:58 +0530

    Add files via upload

---
+++ Cali.py
@@ -0,0 +1,25 @@
+import smtplib
+
+print("I am Cali the multi purpose software ")
+print("Version 2021.3.10")
+print("  ")
+print("For Autometic mail sender type mail")
+
+mailin = str(input())
+
+if mailin == "mail":
+    print("The Email id of reciver")
+    rec = input()
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('pythonmodmail@gmail.com', 'pythonmodmail21')
+    server.sendmail('pythonmodmail@gmail.com',
+                     rec ,
+                    'Hi i am python mod mail made by sir Aadit sharma we are testing this so kindly provide feedback')
+                      
+
+
+
+            
+
+
